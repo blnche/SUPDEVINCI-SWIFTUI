64dce28c1c1e32adf33e58a49a6243c50d40aa18
blnche <peltierblanche@gmail.com>
2026-02-04 14:12:23 +0100

    Initial Commit

---
+++ SUPDEVINCI-SWIFTUI/SUPDEVINCI_SWIFTUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUPDEVINCI_SWIFTUIApp.swift
+//  SUPDEVINCI-SWIFTUI
+//
+//  Created by Blanche Peltier on 04/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct SUPDEVINCI_SWIFTUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SUPDEVINCI-SWIFTUITests/SUPDEVINCI_SWIFTUITests.swift
@@ -0,0 +1,17 @@
+//
+//  SUPDEVINCI_SWIFTUITests.swift
+//  SUPDEVINCI-SWIFTUITests
+//
+//  Created by Blanche Peltier on 04/02/2026.
+//
+
+import Testing
+@testable import SUPDEVINCI_SWIFTUI
+
+struct SUPDEVINCI_SWIFTUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
