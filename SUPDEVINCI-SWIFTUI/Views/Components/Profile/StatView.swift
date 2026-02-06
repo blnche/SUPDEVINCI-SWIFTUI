b81c8d905cbbe27d60a01b48a5c3b288a148c17f
Blnche <peltierblanche@gmail.com>
2026-02-06 11:59:42 +0100

    WIP profile account

---
+++ SUPDEVINCI-SWIFTUI/Views/Components/Profile/StatView.swift
@@ -0,0 +1,22 @@
+//
+//  Stat.swift
+//  SUPDEVINCI-SWIFTUI
+//
+//  Created by Cours on 06/02/2026.
+//
+
+struct StatView: View {
+    let number: String
+    let label: String
+    
+    var body: some View {
+        VStack(spacing: 4) {
+            Text(number)
+                .font(.title3)
+                .fontWeight(.bold)
+            Text(label)
+                .font(.caption)
+                .foregroundColor(.secondary)
+        }
+    }
+}
